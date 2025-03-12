4fdb4477750dd5da2ba651cc65e65168e60ed61d
cchi852 <chris.chiem01@gmail.com>
2025-03-12 19:50:04 +1300

    s3 upload, ai models parse

---
+++ src/app/api/parseMenu/route.ts
@@ -0,0 +1,100 @@
+import { NextResponse } from "next/server";
+import { Together } from "together-ai";
+import { z } from "zod";
+import zodToJsonSchema from "zod-to-json-schema";
+
+const together = new Together({ apiKey: process.env.TOGETHER_API_KEY });
+
+export async function POST(request: Request) {
+    const { menuURL } = await request.json();
+    console.log({ menuURL });
+    if (!menuURL) {
+        return Response.json(
+            { error: "No menu URL provided" },
+            { status: 400 }
+        );
+    }
+    const systemPrompt = `You are given an image of a menu. Your job is to take each item in the menu and convert it into the following JSON format:
+
+    [{"name": "name of menu item", "price": "price of the menu item", "description": "description of menu item"}, ...]
+    
+      Please make sure to include all items in the menu and include a price (if it exists) & a description (if it exists). ALSO PLEASE ONLY RETURN JSON. IT'S VERY IMPORTANT FOR MY JOB THAT YOU ONLY RETURN JSON.
+      `;
+
+    const output = await together.chat.completions.create({
+        model: "meta-llama/Llama-3.2-11B-Vision-Instruct-Turbo",
+        messages: [
+            {
+                role: "user",
+                content: [
+                    { type: "text", text: systemPrompt },
+                    {
+                        type: "image_url",
+                        image_url: {
+                            url: menuURL,
+                        },
+                    },
+                ],
+            },
+        ],
+    });
+    const menuItems = output?.choices[0]?.message?.content;
+
+    // Defining the schema we want our data in
+    const menuSchema = z.array(
+        z.object({
+            name: z.string().describe("The name of the menu item"),
+            price: z.string().describe("The price of the menu item"),
+            description: z
+                .string()
+                .describe(
+                    "The description of the menu item. If this doesn't exist, please write a short one sentence description."
+                ),
+        })
+    );
+
+    const jsonSchema = zodToJsonSchema(menuSchema, "menuSchema");
+    const extract = await together.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content:
+                    "The following is a list of items from a menu. Only answer in JSON.",
+            },
+            {
+                role: "user",
+                content: menuItems!,
+            },
+        ],
+        model: "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
+        // @ts-expect-error - this is not typed in the API
+
+        response_format: { type: "json_object", schema: jsonSchema },
+    });
+    let menuItemsJSON;
+    if (extract?.choices?.[0]?.message?.content) {
+        menuItemsJSON = JSON.parse(extract?.choices?.[0]?.message?.content);
+        console.log({ menuItemsJSON });
+    }
+    // Create an array of promises for parallel image generation
+    try {
+        const imagePromises = menuItemsJSON.map(async (item: any) => {
+            console.log("processing image for:", item.name);
+            const response = await together.images.create({
+                prompt: `A picture of food for a menu, hyper realistic, highly detailed, ${item.name}, ${item.description}.`,
+                model: "black-forest-labs/FLUX.1-schnell-Free",
+                width: 1024,
+                height: 768,
+                steps: 4,
+                response_format: "base64",
+            });
+            item.menuImage = response.data[0];
+            return item;
+        });
+        await Promise.all(imagePromises);
+
+        return Response.json({ menu: menuItemsJSON });
+    } catch (error) {
+        return Response.json({ success: "failed", message: error });
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,135 @@
+"use client";
+import React, { useState } from "react";
+import { useS3Upload } from "next-s3-upload";
+import Dropzone from "react-dropzone";
+import { MagnifyingGlassIcon, PhotoIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+import Input from "./components/Input";
+import { MenuGrid } from "./components/MenuGrid";
+import { ClipLoader } from "react-spinners";
+export interface MenuItem {
+    name: string;
+    price: string;
+    description: string;
+    menuImage: {
+        b64_json: string;
+    };
 }
+
+const Home = () => {
+    const { uploadToS3 } = useS3Upload();
+    const [menuUrl, setMenuUrl] = useState<string | undefined>(undefined);
+    const [status, setStatus] = useState<
+        "initial" | "uploading" | "parsing" | "created" | "failed"
+    >("initial");
+    const [parsedMenu, setParsedMenu] = useState<MenuItem[]>([]);
+
+    const handleFileChange = async (file: File) => {
+        const objectUrl = URL.createObjectURL(file);
+        setStatus("uploading");
+        setMenuUrl(objectUrl);
+        const { url } = await uploadToS3(file);
+        console.log("Successfully uploaded to S3!", url);
+        setStatus("parsing");
+        const res = await fetch("/api/parseMenu", {
+            method: "POST",
+            body: JSON.stringify({
+                menuURL: url,
+            }),
+        });
+        const json = await res.json();
+        if (json.success == "failed") {
+            setStatus("failed");
+            console.log(json.message);
+        } else {
+            setStatus("created");
+            setParsedMenu(json.menu);
+            console.log(json);
+        }
+    };
+
+    return (
+        <div className="mx-auto">
+            <h1 className="text-3xl">{status}</h1>
+
+            <div className="max-w-2xl mx-auto">
+                {status == "initial" && (
+                    <Dropzone
+                        accept={{
+                            "image/*": [".jpg", ".jpeg", ".png"],
+                        }}
+                        multiple={false}
+                        onDrop={(acceptedFiles) =>
+                            handleFileChange(acceptedFiles[0])
+                        }
+                        minSize={1024}
+                        maxSize={3072000}
+                    >
+                        {({ getRootProps, getInputProps, isDragAccept }) => (
+                            <div
+                                className={`mt-2 flex aspect-video cursor-pointer items-center justify-center rounded-lg border-2 border-dashed ${
+                                    isDragAccept
+                                        ? "border-blue-500"
+                                        : "border-gray-300"
+                                }`}
+                                {...getRootProps()}
+                            >
+                                <input {...getInputProps()} />
+                                <div className="text-center">
+                                    <PhotoIcon
+                                        className="mx-auto h-12 w-12 text-gray-300"
+                                        aria-hidden="true"
+                                    />
+                                    <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                                        <label
+                                            htmlFor="file-upload"
+                                            className="relative rounded-md bg-white font-semibold text-gray-800 focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 hover:text-gray-600"
+                                        >
+                                            <p className="text-xl">
+                                                Upload your menu
+                                            </p>
+                                            <p className="mt-1 font-normal text-gray-600">
+                                                or take a picture
+                                            </p>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </Dropzone>
+                )}
+                {menuUrl && (
+                    <div className="my-10 mx-auto flex flex-col items-center">
+                        <Image
+                            width={300}
+                            height={300}
+                            src={menuUrl}
+                            alt="Menu"
+                            className="rounded-lg shadow-md border-2 border-black"
+                        />
+                    </div>
+                )}
+            </div>
+            <div className="max-w-5xl mx-auto">
+                {status == "parsing" && (
+                    <ClipLoader
+                        loading={status == "parsing"}
+                        size={150}
+                        aria-label="Loading Spinner"
+                        data-testid="loader"
+                    />
+                )}
+                {parsedMenu.length > 0 && (
+                    <div className="mt-10">
+                        <h2 className="text-4xl font-bold mb-5">
+                            Menu – {parsedMenu.length} dishes detected
+                        </h2>
+                        <MenuGrid items={parsedMenu} />
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
